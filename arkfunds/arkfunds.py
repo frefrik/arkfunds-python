f2df69876bef6c8b1e7cdbd16ca34b19bed70366
Fredrik Haarstad <fredrik@haarstad.no>
2021-07-24 18:44:11 +0200

    Add docstrings

--- arkfunds/arkfunds.py
+++ arkfunds/arkfunds.py
@@ -48,8 +48,10 @@ class ArkFunds:
 
 
 class ETF(ArkFunds):
+    """Class for accessing ARK ETF data"""
+
     def __init__(self, symbol: str):
-        """ARK ETF class
+        """Initialize
 
         Args:
             symbol (str): ARK ETF symbol
@@ -63,6 +65,14 @@ class ETF(ArkFunds):
             self.yf = None
 
     def profile(self, df: bool = False):
+        """Get ARK ETF profile information
+
+        Args:
+            df (bool, optional): Return pandas.DataFrame. Defaults to False.
+
+        Returns:
+            dict
+        """
         params = {
             "symbol": self.symbol,
         }
@@ -72,7 +82,16 @@ class ETF(ArkFunds):
 
         return self._data(_json, df)
 
-    def holdings(self, date: date = None, df: bool = True):
+    def holdings(self, date: date = None, df: bool = True) -> pd.DataFrame:
+        """Get ARK ETF holdings
+
+        Args:
+            date (date, optional): Fund holding date in ISO 8601 format. Defaults to None.
+            df (bool, optional): Return pandas.DataFrame. Defaults to True.
+
+        Returns:
+            pandas.DataFrame
+        """
         params = {
             "symbol": self.symbol,
             "date": date,
@@ -83,7 +102,16 @@ class ETF(ArkFunds):
 
         return self._data(_json, df)
 
-    def trades(self, period: str = None, df: bool = True):
+    def trades(self, period: str = "1d", df: bool = True):
+        """Get ARK ETF intraday trades
+
+        Args:
+            period (str, optional): Valid periods: 1d, 7d, 1m, 3m, 1y, ytd. Defaults to "1d".
+            df (bool, optional): Return pandas.DataFrame. Defaults to True.
+
+        Returns:
+            pandas.DataFrame
+        """
         params = {
             "symbol": self.symbol,
             "period": period,
@@ -95,6 +123,16 @@ class ETF(ArkFunds):
         return self._data(_json, df)
 
     def news(self, date_from: date = None, date_to: date = None, df: bool = True):
+        """Get ARK ETF news
+
+        Args:
+            date_from (date, optional): From-date in ISO 8601 format. Defaults to None.
+            date_to (date, optional): To-date in ISO 8601 format. Defaults to None.
+            df (bool, optional): Return pandas.DataFrame. Defaults to True.
+
+        Returns:
+            pandas.DataFrame
+        """
         params = {
             "symbol": self.symbol,
             "date_from": date_from,
@@ -107,32 +145,76 @@ class ETF(ArkFunds):
         return self._data(_json, df)
 
     def price(self):
+        """Get current ticker price
+
+        Returns:
+            float
+        """
         if self.yf:
             return self.yf.price
 
     def last_trade(self):
+        """Get last trade date
+
+        Returns:
+            datetime.datetime
+        """
         if self.yf:
             return self.yf.last_trade
 
     def change(self):
+        """Get current price change
+
+        Returns:
+            float
+        """
         if self.yf:
             return self.yf.change
 
     def changep(self):
+        """Get current price change in percent
+
+        Returns:
+            float
+        """
         if self.yf:
             return self.yf.changep
 
     def price_history(self, days_back=7, frequency="d"):
+        """Get historical price data for ticker
+
+        Args:
+            days_back (int, optional): Number of days with history. Defaults to 7.
+            frequency (str, optional): Valid params: 'd' (daily), 'w' (weekly), 'm' (monthly). Defaults to "d".
+
+        Returns:
+            pandas.DataFrame
+        """
         if self.yf:
             return self.yf.get_history(days_back=days_back, frequency=frequency)
 
 
 class Stock(ArkFunds):
-    def __init__(self, symbol):
+    """Class for accessing Stock data"""
+
+    def __init__(self, symbol: str):
+        """Initialize
+
+        Args:
+            symbol (str): Stock ticker
+        """
         super().__init__()
         self.symbol = symbol
 
     def profile(self, df: bool = False):
+        """Get Stock profile information
+
+        Args:
+            df (bool, optional): Return pandas.DataFrame. Defaults to False.
+
+        Returns:
+            dict
+        """
         params = {
             "symbol": self.symbol,
         }
@@ -143,6 +225,14 @@ class Stock(ArkFunds):
         return self._data(_json, df)
 
     def fund_ownership(self, df: bool = True):
+        """Get Stock Fund Ownership
+
+        Args:
+            df (bool, optional): Return pandas.DataFrame. Defaults to True.
+
+        Returns:
+            pandas.DataFrame
+        """
         params = {
             "symbol": self.symbol,
         }
@@ -159,6 +249,17 @@ class Stock(ArkFunds):
         date_to: date = None,
         df: bool = True,
     ):
+        """Get Stock Trades
+
+        Args:
+            direction (str, optional): 'Buy' or 'Sell'. Defaults to None.
+            date_from (date, optional): From-date in ISO 8601 format.. Defaults to None.
+            date_to (date, optional): To-date in ISO 8601 format.. Defaults to None.
+            df (bool, optional): Return pandas.DataFrame. Defaults to True.
+
+        Returns:
+            pandas.DataFrame
+        """
         params = {
             "symbol": self.symbol,
             "direction": [direction.lower() if direction else None],
